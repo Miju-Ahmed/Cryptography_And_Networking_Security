f8eef802d141b0f08a965e0457f53aba247211e2
Miju Chowdhury <miju_chowdhury@Miju.Miju>
2025-08-16 00:04:47 +0600

    Add Code

--- Lab_4.py
+++ Lab_4.py
@@ -1,15 +1,13 @@
 import re
 def normalize(text):
     return re.sub('[^A-Z]','',text.upper())
-def transposition_encrypt(plaintext, width,pad_char='X'):
-    text = normalize(plaintext)
+def transposition_encrypt(text, width, pad_char='X'):
     rem = len(text)%width
     if rem!=0:
         text += pad_char*(width-rem)
     rows = len(text)//width
     matrix = [list(text[i*width:(i+1)*width]) for i in range(rows)]
-    ciphertext = ''.join(matrix[r][c] for c in range(width) for r in range(rows))
-    return ciphertext
+    return ''.join(matrix[r][c] for c in range(width) for r in range(rows))
 def transposition_decrypt(ciphertext, width):
     n = len(ciphertext)
     rows = n//width
@@ -19,15 +17,20 @@ def transposition_decrypt(ciphertext, width):
         for r in range(rows):
             matrix[r][c] = ciphertext[idx]
             idx+=1
-    padded_plain = ''.join(''.join(row) for row in matrix)
-    return padded_plain
+    return ''.join(''.join(row) for row in matrix)
+
 if __name__=="__main__":
     plaintext = "DEPARTMENT OF COMPUTER SCIENCE AND TECHNOLY UNIVERSITY OF RAJSHAHI BANGLADESH"
-    width = int(input("Enter width (number of columns): "))
+    width1 = int(input("Enter first width (number of columns): "))
+    width2 = int(input("Enter second width (number of columns): "))
+    norm_plain=normalize(plaintext)
 
-    cipher = transposition_encrypt(plaintext, width)
-    print("Ciphertext:", cipher)
-    decrypted_padded = transposition_decrypt(cipher, width)
-    normalized_original = normalize(plaintext)
-    decrypted = decrypted_padded[:len(normalized_original)]
+    c1 = transposition_encrypt(norm_plain, width1)
+    print("Ciphertext after single transposition:", c1)
+    c2 = transposition_encrypt(c1, width2)
+    print("Ciphertext after double transposition:", c2)
+
+    d2 = transposition_decrypt(c2, width2)
+    d1 = transposition_decrypt(d2, width1)
+    decrypted = d1[:len(norm_plain)]
     print("Decrypted (original, normalized):", decrypted)
